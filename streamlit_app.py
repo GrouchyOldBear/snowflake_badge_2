86657752628c5dda75352fa053667241543f3acb
GrouchyOldBear <145370323+GrouchyOldBear@users.noreply.github.com>
2023-09-18 10:50:53 -0500

    Update streamlit_app.py
    New stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Diner')
+Streamlit.header('Breakfast Menu')
+Streamlit.text('Omega3 & Oatmeal')
+Streamlit.text('Kale, Spinich & Rocket Smoothie')
+Streamlit.text('Hard-Boiled Free Range Egg')
